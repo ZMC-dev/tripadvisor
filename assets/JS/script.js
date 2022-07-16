c3f0d5fab966a3ca1fa247e2ed1e7413087971af
ZMC-dev <zmc.dev2019@gmail.com>
2022-07-16 16:22:23 +0200

    adding footer

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,23 +1,15 @@
-// console.log("Coucou");
-
 const $ = document;
-
-// Attendre que notre page soit chargée avant de faire quoi que ce soit
 
 $.addEventListener("DOMContentLoaded", () => {
   console.log("Page Chargée");
 
   $.querySelector("#btn-contact-form").addEventListener("click", () => {
-    // Toggle permet de faire en sorte que si la class n'est pas présente on la rajoute, si elle est présente on l'enlève
     $.querySelector(".div-contact-form").classList.toggle("display");
   });
 
   $.querySelector("#contact-form").addEventListener("submit", async (event) => {
-    // Je veux empêcher le rafraichissment de la page
     event.preventDefault();
-    // Je fais une requête axios
     const response = await axios.post("http://localhost:3001/send-email", {
-      // Je construit mon body en allant chercher les contenus de mes inputs (leurs values)
       name: $.querySelector("#name").value,
       email: $.querySelector("#email").value,
       subject: $.querySelector("#subject").value,
--- back/index.js
+++ back/index.js
@@ -1,33 +1,38 @@
 const express = require("express");
+const cors = require("cors");
 const formData = require("form-data");
 const Mailgun = require("mailgun.js");
-const mailgun = new Mailgun(formData);
-
-
-
 require("dotenv").config();
 
-const cors = require("cors");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const mailgun = new Mailgun(formData);
 const mg = mailgun.client({
   username: "Zoila Montes Capa",
   key: process.env.API_SECRET
 });
 
+
+app.get("/", (req, res) => {
+  res.send("server is up");
+});
+
+
 app.post("/send-email", (req, res) => {
   console.log(req.body);
+
   try {
     mg.messages
       .create(process.env.DOMAIN, {
-        from: `${req.body.name} <${req.body.email}>`,
+        from: `${req.body.name} < ${req.body.email}>`,
         to: "zmc.dev2019@gmail.com",
         subject: req.body.subject,
-        text: req.body.text,
+        message: req.body.text,
       })
-      .then((msg) => res.json(msg.message)) // logs response data
+      .then((msg) => res.status(200).json(msg.message)) // logs response data
       .catch((err) => res.json(err.message)); // logs any error
   } catch (error) {
     res.status(400).json(error.message);
